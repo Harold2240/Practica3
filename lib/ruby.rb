aaf910af41d6f2b52ff2d6e2c0dfb3cbe3fbfc1e
Harold Garcia <garciafsharold16@gmail.com>
2024-08-21 15:38:11 -0600

    Practica 3 resuelta

---
+++ lib/ruby.rb
@@ -0,0 +1,15 @@
+def intro array
+    array.sum
+end
+
+def nombre nom
+    if nom.empty?
+        "vacio"
+      else
+        "hello #{nom}"
+      end
+end
+
+def datos? array
+    array.size > 0
+end
